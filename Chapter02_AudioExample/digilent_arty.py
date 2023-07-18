85e1f8bdb82379fba816aa4aafb36399254bf597
Alberto Fahrenkrog <fahrenkrog@outlook.com>
2023-07-18 22:09:25 +0800

    Temporary commit. More logic added, that's all.

--- Chapter02_AudioExample/digilent_arty.py
+++ Chapter02_AudioExample/digilent_arty.py
@@ -94,6 +94,10 @@ class I2S_Tx(Module):
 
         i2s_tx_pins = platform.request('i2s_tx', 0)
 
+        # I2S Interface
+        # Left data word, right data word, ready left, ready right
+        self.i2s_if = Record([('l_data', 32), ('r_data', 32)])
+
         # They say it's not good practice, but we'll do it anyway here, we'll downcount
         # the I2S signals from the 12.288 MHz MCLK 
         self.sync.i2s += [
@@ -104,10 +108,74 @@ class I2S_Tx(Module):
             platform.request('i2s_tx_mclk', 0).eq(cd_i2s.clk),
             i2s_tx_pins.clk.eq(i2s_clk_div[1]),
             i2s_tx_pins.sync.eq(i2s_clk_div[7]),
+            i2s_tx_pins.tx.eq(i2s_tx),
             i2s_mclk.eq(cd_i2s.clk),
-            i2s_sclk.eq(i2s_clk_div[1]),
+            i2s_sclk.eq(~i2s_clk_div[1]),   # According to the reference manual, this should be inverted
             i2s_sync.eq(i2s_clk_div[7]),
         ]
+
+        # But at least we'll add them as constraints
+        platform.add_period_constraint(i2s_mclk, 1e9/12.288e6)
+        platform.add_period_constraint(i2s_sclk, 1e9/(12.288e6 / 4))
+        platform.add_period_constraint(i2s_sync, 1e9/48000)
+
+        # From here on we'll work on the 100MHz domain checking for I2S pulses
+        sclk_delay = Signal(1)
+        sclk_re = Signal(1) # Rising edge
+        sync_delay = Signal(1)
+        sync_re = Signal(1) # Rising edge
+        sync_fe = Signal(1) # Falling edge
+
+        self.sync += [
+            sclk_delay.eq(i2s_sclk),
+            If((i2s_sclk == 1) & (sclk_delay == 0),
+                sclk_re.eq(1)
+            ).Else(
+                sclk_re.eq(0)
+            ),
+            If((i2s_sync == 1) & (sync_delay == 0),
+                sync_re.eq(1)
+            ).Else(
+                sync_re.eq(0)
+            ),
+            If((i2s_sync == 0) & (sync_delay == 1),
+                sync_fe.eq(1)
+            ).Else(
+                sync_fe.eq(0)
+            ),
+        ]
+
+        l_data = Signal(32, reset=0)
+        r_data = Signal(32, reset=0)
+
+        self.sync += [
+            If((sync_re == 1) & (sclk_re == 1),
+               l_data.eq(self.i2s_if.l_data),
+               i2s_tx.eq(0),
+            ),
+            If((sync_fe == 1) & (sclk_re == 1),
+               r_data.eq(self.i2s_if.r_data),
+               i2s_tx.eq(0),
+            ),
+            If((sclk_re == 1) & (i2s_sync == 0),
+               i2s_tx.eq(l_data[31]),
+               l_data[1:32].eq(l_data[0:31])
+            ),
+            If((sclk_re == 1) & (i2s_sync == 1),
+               i2s_tx.eq(r_data[31]),
+               r_data[1:32].eq(r_data[0:31])
+            )
+        ]
+
+        self.sync += [
+            If(sync_re == 1,
+                self.i2s_if.l_data.eq(self.i2s_if.l_data + 1)
+            ),
+            If(sync_fe == 1,
+                self.i2s_if.r_data.eq(self.i2s_if.r_data + 1)
+            )
+        ]
+
 
 # Top Module
 class Top(Module):
